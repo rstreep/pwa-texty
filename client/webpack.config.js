22816a6c1aef587b96dc7a599692722eb0ebc412
rstreep <127055958+rstreep@users.noreply.github.com>
2023-08-06 18:04:16 -0400

    added plugins and implimented asset caching in sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const webpack = require('webpack');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,10 +18,17 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      
       new HtmlWebpackPlugin({
         template: './index.html',
         title: ''
       }),
+
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true,
+        requestTimeout: 1
+      }),
+
       new WebpackPwaManifest({
         name: ,
         short_name: ,
@@ -30,7 +37,19 @@ module.exports = () => {
         theme_color: ,
         start_url: './',
         publicPath: './',
-      })
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js'
+      }),
     ],
 
     module: {
